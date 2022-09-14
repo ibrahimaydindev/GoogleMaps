d116d19e09ec812c92108b75eaa36732836a8bda
ibrahim <aydinibrahim1692@gmail.com>
2022-09-14 15:03:16 +0300

    initial commit

---
+++ app/src/main/java/com/example/googlemaps/MapsActivity.kt
@@ -0,0 +1,39 @@
+package com.example.googlemaps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.googlemaps.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+
+        val istanbul = LatLng(41.015137, 28.979530)
+        mMap.addMarker(MarkerOptions().position(istanbul).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(istanbul))
+    }
+}
